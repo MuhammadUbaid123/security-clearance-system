44daf7cc82d263c8f5f9777585260708ba585b9c
muhammad <muhammadubaidn@gmail.com>
2023-05-13 20:12:38 +0500

    Approved Reques or Reject

--- app/Http/Controllers/Api/CleranceRequestController.php
+++ app/Http/Controllers/Api/CleranceRequestController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\ApprovedBy;
 use App\Models\ClearanceRequest;
 use App\Models\Notification;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class CleranceRequestController extends Controller
@@ -41,10 +43,15 @@ class CleranceRequestController extends Controller
                 'message' => 'Already requested for Clearance!',
             ],409);
         }
+
+        $req_to_members = User::select(DB::raw('count(distinct user_type)'))
+        ->whereNotIn('user_type', ['student', 'staff'])
+        ->count();
+
         $data = ClearanceRequest::create([
             'session' => $request->st_session,
             'requester_id' => $authUser?->id,
-            'req_to_members' => 0,
+            'req_to_members' => $req_to_members??0,
             'approvedd_by_count' => 0,
         ]);
 
@@ -123,6 +130,72 @@ class CleranceRequestController extends Controller
             'type'=> 'error',
             'message' => 'Operation Could not perform!',
         ],501);
+
+    }
+
+
+    /* Approve clearance request */
+    public function actionOnClearaceReq(Request $request)
+    {
+        $authUser = Auth::user();
+        $validator = Validator::make($request->all(), [
+            'request_id' => 'required'
+        ]);
+
+        if($validator->fails())
+        {
+            return response()->json([
+                'status_code' => 400,
+                'type'=> 'error',
+                'message' => $validator->messages()->toArray(),
+            ],400);
+        }
+
+        $reqdata = ClearanceRequest::where('id', $request->request_id)->where('')->first();
+
+        if($reqdata)
+        {
+            $data = ApprovedBy::create([
+                'user_id' => $reqdata->requester_id,
+                'name' => $authUser->fname." ".$authUser->lname,
+                'clear_req_id' => $reqdata->id,
+                'comments' => $request->comments,
+                'status' => $request->status,
+            ]);
+
+            if($data)
+            {
+                if($data->status == 1)
+                {
+                    $reqdata->approvedd_by_count++;
+                    $reqdata->save();
+                }
+
+                if($reqdata->approvedd_by_count == $reqdata->req_to_members)
+                {
+                    $reqdata->request_status = 1;
+                    $reqdata->save();
+                }
+                return response()->json([
+                    'status_code' => 200,
+                    'type'=> 'success',
+                    'message' => 'Operation Performed Successfully!',
+                ],200);
+            }
+
+
+            return response()->json([
+                'status_code' => 501,
+                'type'=> 'error',
+                'message' => 'Operation could not Perform!',
+            ],501);
+        }
+
+        return response()->json([
+            'status_code' => 404,
+            'type'=> 'error',
+            'message' => 'Request Not Found!',
+        ],404);
 
     }
 }
